c820cb485518b15b5eac7d91f086f3be90e8cdf5
Z4ffarani <zaffarani.kaique@gmail.com>
2025-05-07 19:36:59 +0000

    moving cors print

--- back-end/src/main/java/com/starbank/config/WebConfig.java
+++ back-end/src/main/java/com/starbank/config/WebConfig.java
@@ -8,10 +8,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     private final String allowedOrigin = System.getenv().getOrDefault("FRONTEND_URL", "http://localhost:3000");
-    System.out.println("CORS allowed origin: " + allowedOrigin);
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        System.out.println("CORS allowed origin: " + allowedOrigin);
+
         registry.addMapping("/**")
                 .allowedOrigins(allowedOrigin)
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
